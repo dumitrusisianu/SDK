7d4bd1b2d478ca86a1212adfaa991313fb95ae31
Dumitru Sisianu <intexteel@gmail.com>
2019-09-17 01:09:57 +0300

    [DEV] - Minor fixes & clean-up.

--- XRTK.SDK/Packages/com.xrtk.sdk/Features/UX/Scripts/Pointers/LinePointer.cs
+++ XRTK.SDK/Packages/com.xrtk.sdk/Features/UX/Scripts/Pointers/LinePointer.cs
@@ -89,6 +89,7 @@ namespace XRTK.SDK.UX.Pointers
 
         protected override void OnEnable()
         {
+            Debug.LogError("LinePointer Enabled!!");
             base.OnEnable();
             CheckInitialization();
         }
